cb8a44cc272b4ee7dfb6c00d75f37bf9c124d9cf
Mike Heath <elcapo@gmail.com>
2013-04-01 15:10:12 -0600

    Added unique id field per https://github.com/cloudfoundry/cloud_controller_ng/commit/fad6072c4d2774ae2bacf61fe491230228f1cc63

--- vcap-service-gateway/src/main/java/vcap/service/CreateRequest.java
+++ vcap-service-gateway/src/main/java/vcap/service/CreateRequest.java
@@ -7,6 +7,9 @@ import vcap.common.JsonObject;
  * @author Mike Heath <elcapo@gmail.com>
  */
 public class CreateRequest extends JsonObject {
+	private static final String SPACE_GUID = "space_guid";
+	private static final String ORGANIZATION_GUID = "organization_guid";
+	private static final String UNIQUE_ID = "unique_id";
 	private final String label;
 	private final String name;
 	private final String email;
@@ -15,6 +18,7 @@ public class CreateRequest extends JsonObject {
 	private final String provider;
 	private final String spaceGuid;
 	private final String organizationGuid;
+	private final String uniqueId;
 
 	public CreateRequest(
 			@JsonProperty("label") String label,
@@ -23,8 +27,9 @@ public class CreateRequest extends JsonObject {
 			@JsonProperty("plan") String plan,
 			@JsonProperty("version") String version,
 			@JsonProperty("provider") String provider,
-			@JsonProperty("space_guid") String spaceGuid,
-			@JsonProperty("organization_guid") String organizationGuid) {
+			@JsonProperty(SPACE_GUID) String spaceGuid,
+			@JsonProperty(ORGANIZATION_GUID) String organizationGuid,
+			@JsonProperty(UNIQUE_ID) String uniqueId) {
 		this.label = label;
 		this.name = name;
 		this.email = email;
@@ -33,6 +38,7 @@ public class CreateRequest extends JsonObject {
 		this.provider = provider;
 		this.spaceGuid = spaceGuid;
 		this.organizationGuid = organizationGuid;
+		this.uniqueId = uniqueId;
 	}
 
 	public String getLabel() {
@@ -59,11 +65,18 @@ public class CreateRequest extends JsonObject {
 		return provider;
 	}
 
+	@JsonProperty(SPACE_GUID)
 	public String getSpaceGuid() {
 		return spaceGuid;
 	}
 
+	@JsonProperty(ORGANIZATION_GUID)
 	public String getOrganizationGuid() {
 		return organizationGuid;
+	}
+
+	@JsonProperty(UNIQUE_ID)
+	public String getUniqueId() {
+		return uniqueId;
 	}
 }
