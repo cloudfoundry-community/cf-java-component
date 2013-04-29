89361c49e1b124b8a716dfd5b94ac73016c3d44a
Mike Heath <elcapo@gmail.com>
2013-04-29 10:28:57 -0600

    Cleaned up imports.

--- vcap-client/src/test/java/ClientTest.java
+++ vcap-client/src/test/java/ClientTest.java
@@ -1,11 +1,9 @@
 import org.apache.http.impl.client.DefaultHttpClient;
+import vcap.client.CfTokens;
 import vcap.client.CloudController;
-import vcap.client.Resource;
-import vcap.client.RestCollection;
-import vcap.client.model.Info;
-import vcap.client.Token;
+import vcap.client.TokenContents;
 import vcap.client.Uaa;
-import vcap.client.model.Service;
+import vcap.client.model.Info;
 
 /**
  * @author Mike Heath <elcapo@gmail.com>
@@ -18,13 +16,9 @@ public class ClientTest {
 
 		final Uaa uaa = cloudController.getUaa();
 //		final Uaa uaa = new Uaa(new DefaultHttpClient(), "https://uaa.app2-dev.lds.org");
-		final Token token = uaa.getClientToken("service-gateway", "gatewaysecret");
 
-		final RestCollection<Service> services = cloudController.getServices(token);
-		for (Resource<Service> service : services) {
-			System.out.println("Label: " + service.getEntity().getLabel());
-			System.out.println("UUID: " + service.getGuid());
-			System.out.println();
-		}
+		final CfTokens tokens = new CfTokens();
+		final TokenContents tokenContents = uaa.checkToken("servicegateway", "gatewaysecret", tokens.getTargetToken().getToken());
+		System.out.println(tokenContents.getEmail());
 	}
 }
