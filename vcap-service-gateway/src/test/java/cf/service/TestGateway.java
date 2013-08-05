af7102d29675840f0062ec1339172bb770e7c46c
Mike Heath <elcapo@gmail.com>
2013-08-05 16:11:18 -0600

    Updated copyright headers.

--- vcap-service-gateway/src/test/java/cf/service/TestGateway.java
+++ vcap-service-gateway/src/test/java/cf/service/TestGateway.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright (c) 2013 Intellectual Reserve, Inc.  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package cf.service;
 
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -15,6 +31,8 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * Used for reverse engineering REST calls from Cloud Controller.
+ *
  * @author Mike Heath <elcapo@gmail.com>
  */
 public class TestGateway {
