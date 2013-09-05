fbd53ed7e1098446bb6fb652ab252f27b0499074
Mike Heath <elcapo@gmail.com>
2013-09-05 10:29:03 -0600

    Added support for using YAML properties with Spring Boot.

---
+++ cf-spring/src/main/java/cf/spring/YamlPropertyContextInitializer.java
@@ -0,0 +1,69 @@
+/*
+ *   Copyright (c) 2013 Intellectual Reserve, Inc.  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package cf.spring;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+/**
+ * @author Mike Heath <elcapo@gmail.com>
+ */
+public class YamlPropertyContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+	private final String name;
+	private final String locationProperty;
+	private final String locationDefault;
+	private final boolean addFirst;
+
+	public YamlPropertyContextInitializer(String name, String locationProperty, String locationDefault) {
+		this(name, locationProperty, locationDefault, true);
+	}
+
+	public YamlPropertyContextInitializer(String name, String locationProperty, String locationDefault, boolean addFirst) {
+		this.name = name;
+		this.locationProperty = locationProperty;
+		this.locationDefault = locationDefault;
+		this.addFirst = addFirst;
+	}
+
+	@Override
+	public void initialize(ConfigurableApplicationContext applicationContext) {
+		try {
+			final ConfigurableEnvironment environment = applicationContext.getEnvironment();
+			final Resource resource = applicationContext.getResource(environment.getProperty(locationProperty, locationDefault));
+			final YamlDocument yamlDocument;
+			yamlDocument = YamlDocument.load(resource);
+			final MutablePropertySources propertySources = environment.getPropertySources();
+			final PropertySource propertySource = new YamlPropertySource(name, yamlDocument);
+			if (addFirst) {
+				propertySources.addFirst(propertySource);
+			} else {
+				propertySources.addLast(propertySource);
+			}
+
+			applicationContext.getBeanFactory().registerSingleton(name, yamlDocument);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ cf-spring/src/test/java/cf/spring/YamlPropertyContextInitializerTest.java
@@ -0,0 +1,65 @@
+/*
+ *   Copyright (c) 2013 Intellectual Reserve, Inc.  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package cf.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * @author Mike Heath <elcapo@gmail.com>
+ */
+public class YamlPropertyContextInitializerTest {
+
+	@Configuration
+	public static class Config {}
+
+	@Test
+	public void  contextWithYamlProperties() {
+		final String name = "config";
+
+		final SpringApplication springApplication = new SpringApplication(Config.class);
+		springApplication.addInitializers(new YamlPropertyContextInitializer(name, "config", "testProperties.yml"));
+		try (ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) springApplication.run()) {
+			final YamlDocument config = applicationContext.getBean(name, YamlDocument.class);
+			assertNotNull(config);
+			assertEquals(config.get("foo"), "This is foo");
+
+			final Environment environment = applicationContext.getEnvironment();
+			assertEquals(environment.getProperty("foo"), "This is foo");
+		}
+	}
+
+	@Test(dependsOnMethods = "contextWithYamlProperties")
+	public void  contextWithAlternateYamlProperties() {
+		final String property = "config";
+		final String name = "alternate";
+
+		System.setProperty(property, "alternateConfig.yml");
+
+		final SpringApplication springApplication = new SpringApplication(Config.class);
+		springApplication.addInitializers(new YamlPropertyContextInitializer(name, property, "testProperties.yml"));
+		try (ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) springApplication.run()) {
+			final YamlDocument config = applicationContext.getBean(name, YamlDocument.class);
+			assertNotNull(config);
+			assertEquals(config.get("foo"), "bar");
+		}
+	}
+}
